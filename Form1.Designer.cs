ae0db9b30716b1c213894cd8eb99b47e84c1ed6e
zhangxv_0 <8153942+zhangxv0@user.noreply.gitee.com>
2021-07-07 14:24:07 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace wuziqi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.开始游戏ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.游戏步骤ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.读取ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.设置棋盘颜色ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(26, 20);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(450, 480);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 503);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem,
+            this.设置ToolStripMenuItem,
+            this.帮助ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(512, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.开始游戏ToolStripMenuItem,
+            this.游戏步骤ToolStripMenuItem,
+            this.保存ToolStripMenuItem,
+            this.读取ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // 开始游戏ToolStripMenuItem
+            // 
+            this.开始游戏ToolStripMenuItem.Name = "开始游戏ToolStripMenuItem";
+            this.开始游戏ToolStripMenuItem.Size = new System.Drawing.Size(152, 26);
+            this.开始游戏ToolStripMenuItem.Text = "开始游戏";
+            this.开始游戏ToolStripMenuItem.Click += new System.EventHandler(this.开始游戏ToolStripMenuItem_Click);
+            // 
+            // 游戏步骤ToolStripMenuItem
+            // 
+            this.游戏步骤ToolStripMenuItem.Name = "游戏步骤ToolStripMenuItem";
+            this.游戏步骤ToolStripMenuItem.Size = new System.Drawing.Size(152, 26);
+            this.游戏步骤ToolStripMenuItem.Text = "游戏步骤";
+            // 
+            // 保存ToolStripMenuItem
+            // 
+            this.保存ToolStripMenuItem.Name = "保存ToolStripMenuItem";
+            this.保存ToolStripMenuItem.Size = new System.Drawing.Size(152, 26);
+            this.保存ToolStripMenuItem.Text = "保存";
+            // 
+            // 读取ToolStripMenuItem
+            // 
+            this.读取ToolStripMenuItem.Name = "读取ToolStripMenuItem";
+            this.读取ToolStripMenuItem.Size = new System.Drawing.Size(152, 26);
+            this.读取ToolStripMenuItem.Text = "读取";
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(152, 26);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // 设置ToolStripMenuItem
+            // 
+            this.设置ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.设置棋盘颜色ToolStripMenuItem});
+            this.设置ToolStripMenuItem.Name = "设置ToolStripMenuItem";
+            this.设置ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.设置ToolStripMenuItem.Text = "设置";
+            // 
+            // 设置棋盘颜色ToolStripMenuItem
+            // 
+            this.设置棋盘颜色ToolStripMenuItem.Name = "设置棋盘颜色ToolStripMenuItem";
+            this.设置棋盘颜色ToolStripMenuItem.Size = new System.Drawing.Size(182, 26);
+            this.设置棋盘颜色ToolStripMenuItem.Text = "设置棋盘颜色";
+            this.设置棋盘颜色ToolStripMenuItem.Click += new System.EventHandler(this.设置棋盘颜色ToolStripMenuItem_Click);
+            // 
+            // 帮助ToolStripMenuItem
+            // 
+            this.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem";
+            this.帮助ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.帮助ToolStripMenuItem.Text = "帮助";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 522);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "五子棋";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 开始游戏ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 游戏步骤ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 读取ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 设置棋盘颜色ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wuziqi
+{
+    public partial class Form1 : Form
+    {
+        Graphics graphics;
+        const int num_che = 20;
+        int[,] ChessBack = new int[num_che, num_che];
+        //双方轮换,true是红，false是蓝
+        static bool type;
+        //是否开始
+        static bool start;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Width = MainSize.Wid;
+            this.Height = MainSize.Hei;
+            this.pictureBox1.Width = MainSize.CBWid;
+            this.pictureBox1.Height = MainSize.CBHei;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeThis();
+        }
+        private void InitializeThis()
+        {
+            for (int i = 0; i < num_che; i++)
+            {
+                for (int j = 0; j < num_che; j++)
+                    ChessBack[i, j] = 0;
+            }
+            start = false;
+            this.label1.Text = "游戏尚未开始";
+            ChessBoard.DrawCB(graphics, this.pictureBox1);
+            type = true;
+            //btnStart.Enabled = true;
+            //btnReset.Enabled = false;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (start)
+            {
+                //在计算矩阵中的位置
+                int bX = (int)((e.X + MainSize.CBGap / 2) / MainSize.CBGap);
+                int bY = (int)((e.Y + MainSize.CBGap / 2) / MainSize.CBGap);
+                //防止在同一个位置落子
+                if (ChessBack[bX, bY] != 0)
+                    return;
+                Chess.DrawChess(type, pictureBox1, graphics, e);
+                ChessBack[bX, bY] = type ? 1 : 2;
+                //判断棋盘是否满了
+
+                if (IsFull() && !Victory(bX, bY))
+                {
+                    if (MessageBox.Show("游戏结束，平局") == DialogResult.OK)
+                        InitializeThis();
+                    return;
+                }
+                //判断胜利
+                if (Victory(bX, bY))
+                {
+                    string Vic = type ? "黑" : "白";
+                    if (MessageBox.Show(Vic + "方胜利!") == DialogResult.OK)
+                        InitializeThis();
+                    return;
+                }
+
+                //换人
+                type = !type;
+                label1.Text = type ? "黑方's trun!" : "白方's turn!";
+            }
+            else {
+                MessageBox.Show("游戏尚未开始");
+                return; }
+        }
+        /// <summary>
+        /// 判断棋盘是否满
+        /// </summary>
+        /// <returns></returns>
+        private bool IsFull()
+        {
+            bool full = true;
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    if (ChessBack[i, j] == 0)
+                        full = false;
+                }
+            }
+            return full;
+        }
+        #region 判断胜利
+        private bool Victory(int bx, int by)
+        {
+            if (HorVic(bx, by))
+                return true;
+            if (VerVic(bx, by))
+                return true;
+            if (Vic45(bx, by))
+                return true;
+            else
+                return false;
+        }
+
+        private bool Vic45(int bx, int by)
+        {
+
+            int b1 = (bx - 4) > 0 ? bx - 4 : 0;
+            int b2 = (by - 4) > 0 ? by - 4 : 0;
+            //int buttom = b1 > b2 ? b2 : b1;
+            int val = ChessBack[bx, by];
+            for (int i = b1, j = b2; i < 16 && j < 16; i++, j++)
+            {
+                if (ChessBack[i, j] == val && ChessBack[i + 1, j + 1] == val &&
+                    ChessBack[i + 2, j + 2] == val && ChessBack[i + 3, j + 3] == val
+                    && ChessBack[i + 4, j + 4] == val)
+                    return true;
+            }
+            for (int i = b1, j = b2; i < 16 && j < 16; i++, j++)
+            {
+                if (ChessBack[i, j] == val && ChessBack[i + 1, j - 1] == val &&
+                    ChessBack[i + 2, j - 2] == val && ChessBack[i + 3, j - 3] == val
+                    && ChessBack[i - 4, j - 4] == val)
+                    return true;
+            }
+            return false;
+        }
+
+        private bool VerVic(int bx, int by)
+        {
+            int buttom = (by - 4) > 0 ? by - 4 : 0;
+            int val = ChessBack[bx, by];
+            for (int i = buttom; i < 16; i++)
+            {
+                if (ChessBack[bx, i] == val && ChessBack[bx, i + 1] == val &&
+                    ChessBack[bx, i + 2] == val && ChessBack[bx, i + 3] == val
+                    && ChessBack[bx, i + 4] == val)
+                    return true;
+            }
+            return false;
+        }
+
+        private bool HorVic(int bx, int by)
+        {
+            int left = (bx - 4) > 0 ? bx - 4 : 0;
+            int val = ChessBack[bx, by];
+            for (int i = left; i < 16; i++)
+            {
+                if (ChessBack[i, by] == val && ChessBack[i + 1, by] == val &&
+                    ChessBack[i + 2, by] == val && ChessBack[i + 3, by] == val
+                    && ChessBack[i + 4, by] == val)
+                    return true;
+            }
+            return false;
+        }
+
+        #endregion
+
+        private void 开始游戏ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            start = true;
+            this.label1.Text = "开始游戏";
+            MessageBox.Show("游戏开始!!!!!");
+        }
+        Color color = Color.Black;
+        private void 设置棋盘颜色ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!start)
+            {
+                Color color;
+                DialogResult dialogResult = colorDialog1.ShowDialog();
+                if (dialogResult == DialogResult.OK)
+                {
+                    color = colorDialog1.Color;
+                    ChessBoard.Color1 = color;
+                    ChessBoard.DrawCB(graphics, this.pictureBox1);
+                }
+            }
+            else
+            {
+                MessageBox.Show("只有在游戏没有开始时才能设置棋盘颜色");
+                return;
+            }
+            
+
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+
+
+
+
+    /// <summary>
+    /// 棋盘类
+    /// </summary>
+    class ChessBoard
+    {
+
+        static Color color = Color.Black;
+        static readonly float penWid = 1.0f;
+        static readonly Pen pen = new Pen(color, penWid);
+        static public Color Color1 = Color.White;
+
+        public static void DrawCB(Graphics gra, PictureBox pic)
+        {
+            //每排数量
+            int horC = MainSize.CBWid / MainSize.CBGap;
+            //间隔
+            int gap = MainSize.CBGap;
+            Image img = new Bitmap(MainSize.CBWid, MainSize.CBHei);
+            gra = Graphics.FromImage(img);
+            gra.Clear(Color1);
+            gra.DrawRectangle(pen, 0, 0, MainSize.CBWid, MainSize.CBHei);
+            //画棋盘
+            for (int i = 0; i < horC; i++)
+            {
+                gra.DrawLine(pen, 0, i * gap, MainSize.CBWid, i * gap);
+                gra.DrawLine(pen, i * gap, 0, i * gap, MainSize.CBHei);
+            }
+            //pic.BackColor = Color.Red;
+            gra.DrawLine(pen, 0, horC * gap, MainSize.CBWid, horC * gap - 1);
+            gra.DrawLine(pen, horC * gap - 1, 0, horC * gap, MainSize.CBHei);
+            pic.Image = img;
+        }
+
+    }
+    /// <summary>
+    /// 期盼的基本参数
+    /// </summary>
+    class MainSize
+    {
+        //主框体大小
+        public static int Wid { get { return 520; } }
+        public static int Hei { get { return 460; } }
+        //棋盘大小
+        public static int CBWid { get { return 401; } }
+        public static int CBHei { get { return 401; } }
+        //棋盘宽度
+        public static int CBGap { get { return 20; } }
+        //棋子直径
+        public static int ChessRadious { get { return 16; } }
+    }
+    /// <summary>
+    /// 棋子类
+    /// </summary>
+    class Chess
+    {
+        public static void DrawChess(bool type, PictureBox pic, Graphics graphic, MouseEventArgs e)
+        {
+            graphic = pic.CreateGraphics();
+            Pen pen1 = new Pen(Color.Black, 1);
+            Brush bru1 = new SolidBrush(Color.Black);
+            Pen pen2 = new Pen(Color.Black, 1);
+            Brush bru2 = new SolidBrush(Color.White);
+            int newX = (int)((e.X + MainSize.CBGap / 2) / MainSize.CBGap) * MainSize.CBGap - MainSize.ChessRadious / 2;
+            int newY = (int)((e.Y + MainSize.CBGap / 2) / MainSize.CBGap) * MainSize.CBGap - MainSize.ChessRadious / 2;
+            if (type)
+            {
+                graphic.DrawEllipse(pen1, newX, newY, MainSize.ChessRadious, MainSize.ChessRadious);
+                graphic.FillEllipse(bru1, newX, newY, MainSize.ChessRadious, MainSize.ChessRadious);
+            }
+            if (!type)
+            {
+                graphic.DrawEllipse(pen2, newX, newY, MainSize.ChessRadious, MainSize.ChessRadious);
+                graphic.FillEllipse(bru2, newX, newY, MainSize.ChessRadious, MainSize.ChessRadious);
+            }
+            graphic.Dispose();
+        }
+
+    }
+}
